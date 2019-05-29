4e9ae8a1704ad7335e166754b95feeda202a25e9
jessiLeandro <jessi.profissional@gmail.com>
2019-05-29 10:24:20 -0300

    equipType: controllers, test and router

---
+++ src/controllers/equip/equipType/index.js
@@ -0,0 +1,35 @@
+const EquipTypeDomain = require('../../../domains/equip/equipType')
+const database = require('../../../database')
+
+const equipTypeDomain = new EquipTypeDomain()
+
+const add = async (req, res, next) => {
+  const transaction = await database.transaction()
+  try {
+    const equipType = await equipTypeDomain.add(req.body, { transaction })
+
+    await transaction.commit()
+    res.json(equipType)
+  } catch (error) {
+    await transaction.rollback()
+    next(error)
+  }
+}
+
+const getAll = async (req, res, next) => {
+  const transaction = await database.transaction()
+  try {
+    const equipType = await equipTypeDomain.getAll()
+
+    await transaction.commit()
+    res.json(equipType)
+  } catch (error) {
+    await transaction.rollback()
+    next()
+  }
+}
+
+module.exports = {
+  add,
+  getAll,
+}
--- src/routes/protect/index.js
+++ src/routes/protect/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router({ mergeParams: true })
 const companyRoute = require('./company')
+const equipTypeRoute = require('./equipType')
 
 router.use('/company', companyRoute)
+router.use('/equip/equipType', equipTypeRoute)
 
 module.exports = router
