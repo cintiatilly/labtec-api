2bed2ed75993a87f014289631392d158522080d4
jessiLeandro <jessi.profissional@gmail.com>
2019-05-31 14:58:24 -0300

    routers

--- src/routes/protect/index.js
+++ src/routes/protect/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router({ mergeParams: true })
 const companyRoute = require('./company')
 const equipTypeRoute = require('./equipType')
+const equipRoute = require('./equip')
+
 
 router.use('/company', companyRoute)
 router.use('/equip/equipType', equipTypeRoute)
+router.use('/equip', equipRoute)
 
 module.exports = router
