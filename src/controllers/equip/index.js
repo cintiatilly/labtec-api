228e62a82fc62350ac09803af0888d9364718c22
jessiLeandro <jessi.profissional@gmail.com>
2019-05-31 15:01:09 -0300

    controllers and test

---
+++ src/controllers/equip/index.js
@@ -0,0 +1,49 @@
+const EquipDomain = require('../../domains/equip')
+const database = require('../../database')
+
+const equipDomain = new EquipDomain()
+
+const add = async (req, res, next) => {
+  const transaction = await database.transaction()
+  try {
+    const equip = await equipDomain.add(req.body, { transaction })
+
+    await transaction.commit()
+    res.json(equip)
+  } catch (error) {
+    await transaction.rollback()
+    next(error)
+  }
+}
+
+const getAll = async (req, res, next) => {
+  const transaction = await database.transaction()
+  try {
+    const equip = await equipDomain.getAll()
+
+    await transaction.commit()
+    res.json(equip)
+  } catch (error) {
+    await transaction.rollback()
+    next()
+  }
+}
+
+const getOneBySerialNumber = async (req, res, next) => {
+  const transaction = await database.transaction()
+  try {
+    const equip = await equipDomain.getOneBySerialNumber('123456789')
+
+    await transaction.commit()
+    res.json(equip)
+  } catch (error) {
+    await transaction.rollback()
+    next()
+  }
+}
+
+module.exports = {
+  add,
+  getAll,
+  getOneBySerialNumber,
+}
