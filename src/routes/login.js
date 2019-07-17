66bf5a0f56820e7dbf7cc5f5285666ea120f41a8
jessiLeandro <jessi.profissional@gmail.com>
2019-07-17 16:58:17 -0300

    ADD: sessão -  controller

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,7 @@
 const router = require('express').Router({ mergeParams: true })
 const loginController = require('../controllers/login')
 
+router.get('/auth', loginController.checkSessionIsValid)
 router.post('/login', loginController.loginController)
 router.delete('/logout', loginController.logoutController)
 
